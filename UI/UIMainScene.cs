7f025920824dd0561eb5c3229f04bc3346895d5c
manokimbr <kim.souza.2201@gmail.com>
2021-11-10 11:44:28 -0300

    Scene management

--- UI/UIMainScene.cs
+++ UI/UIMainScene.cs
@@ -73,4 +73,8 @@ public class UIMainScene : MonoBehaviour
             InfoPopup.Name.text = content.GetName();
         }
     }
+    public void handleBackToMenuClick()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
